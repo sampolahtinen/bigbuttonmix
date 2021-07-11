2b21068fd50648fd41f5a11d41a19bc64b86f257
Sampo Lahtinen <lampo.sahtinen@gmail.com>
2021-07-11 13:57:10 +0200

    add bit more error handling

--- client/src/components/BigButton.svelte
+++ client/src/components/BigButton.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <button class="big-button" class:is-small={isSmall} class:is-loading={isLoading} on:click>
-  <span>Whats cooking?</span>
+  <span>GO!</span>
 </button>
 
 <style>
@@ -16,7 +16,7 @@
     border: none;
     cursor: pointer;
     background: unset;
-    font-size: 2em;
+    font-size: 2.5em;
     color: white;
     text-transform: uppercase;
     border: 2px solid rgb(255, 85, 85);
@@ -24,8 +24,6 @@
   }
 
   .is-small {
-    /* width: 100px;
-    height: 100px; */
     transform: scale(0.7);
   }
 
@@ -40,12 +38,15 @@
   @keyframes pulse {
   0% {
     border-color: #FFF;
+    color: #FFF;
   }
   50% {
     border-color: #ff403670;
+    color: #ff403670;
   }
   100% {
     border-color: #FF4136;
+    color: #FF4136;
   }
 }
 </style>
