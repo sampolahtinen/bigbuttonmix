7975263075982d730c461de5aa83ea918dbd8578
AndyMoore <andrew.moore@auroraer.com>
2021-08-05 16:26:03 +0200

    build commit

--- tests/test_request_library.py
+++
@@ -1,8 +0,0 @@
-from bigbutton.request_library import *
-import pytest
-
-
-def test_request_hidden():
-    google = r'https://www.google.com/'
-    r = request_hidden(google)
-    assert r.status_code == 200
--- tests/tests_bigbutton_main.py
+++
@@ -1,6 +0,0 @@
-from bigbutton.bigbutton_main import *
-import pytest
-
-def test_bigbutton():
-    test_url = r'https://ra.co/events/uk/london?week=2021-05-20'
-    soundcloud_url = bigbutton_search(test_url)
