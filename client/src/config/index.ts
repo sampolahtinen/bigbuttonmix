339c3a9b6b1e41a691791cd0157403db1c40054a
Andy <abmoore92@gmail.com>
2021-08-07 18:55:54 +0200

    hard code server production location

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,7 @@
 export const CONFIG = {
-    baseUrl: 'http://localhost:4000'
+    //baseUrl: 'http://localhost:4000'
+    baseUrl: 'https://bigbuttonmix-server.vercel.app/'
+    
 }
 
 export const __DEV__ = process.env.NODE_ENV === 'development'
