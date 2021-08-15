fadcef5dc12a249ebd896d1eb38f302087e3dd93
Sampo Lahtinen <sampo.lahtinen@endava.com>
2021-08-16 00:20:30 +0300

    build static

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,8 +1,7 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -16,7 +15,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
