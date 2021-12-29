54502528041eed06d938dbedbfb65d69a1d47dc0
Andy <abmoore92@gmail.com>
2021-12-29 14:03:09 +0100

    improve logging on weekly clear cache task

--- api/tasks/weekly.ts
+++ api/tasks/weekly.ts
@@ -8,16 +8,18 @@ logInfo("Running weekly tasks")
 
 
 const deleteCache = async() => {
-    const dbMemoryInfo = await redisClient.info('memory')
-    logInfo(`Redis cache is being cleared`)
-
-    logInfo(dbMemoryInfo)
+    const dbMemoryInfoStart = await redisClient.info('memory')
+    logInfo(`Redis cache is being cleared:`)
+    logInfo(`Memory info before clearing:`)
+    logInfo(dbMemoryInfoStart)
 
     const reply = await redisClient.flushdb();  // delete redis database
     logInfo(`Redis response: ${reply}`)
 
-    logInfo(`Redis cache is clear`)
-
+    const dbMemoryInfoEnd = await redisClient.info('memory')
+    logInfo(`Redis clearing is finished`)
+    logInfo(`Memory info after clearing:`)
+    logInfo(dbMemoryInfoEnd)   
 }
 
 const runWeeklyJobs = async() => {
