261f1e00272c3379be17ff41727907a8b3cd6384
Sampo Lahtinen <sampo.lahtinen@endava.com>
2022-01-07 17:37:18 +0100

    [housekeeping] remove commented code & move apiUrl

--- client/config/apollo/index.ts
+++ client/config/apollo/index.ts
@@ -1,7 +1,11 @@
-import { apiUrl } from '../../src/api/index';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+export const graphqlEndpoint =
+  process.env.NODE_ENV === 'production'
+    ? 'https://big-button-api.herokuapp.com/graphql'
+    : 'http://localhost:5000/api';
+
 export const apolloClient = new ApolloClient({
-  uri: __DEV__ ? 'http://localhost:4000/graphql' : apiUrl,
+  uri: __DEV__ ? 'http://localhost:4000/graphql' : graphqlEndpoint,
   cache: new InMemoryCache()
 });
