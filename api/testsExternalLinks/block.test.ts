b0d16ef3e336f2195e98949575b441f4bc32b14e
Andy <abmoore92@gmail.com>
2022-01-10 22:51:28 +0100

    added block test to selecting random soundcloud track

--- api/testsExternalLinks/block.test.ts
+++ api/testsExternalLinks/block.test.ts
@@ -1,10 +1,15 @@
 import { 
-    getRandomRaEventArtists
+    getRandomRaEventArtists,
+    getRandomSoundcloudTrack
 } from '../src/utils/raScraper';
 import { Crawler } from "../src/utils/Crawler";
 
+const testArtistScPage = "https://soundcloud.com/answercoderequest"
+const errorUrl = 'https://www.google.com' 
+
 
 jest.setTimeout(15000)    
+
 test('Scraper: random artist from search url', async () =>{
     const crawler = new Crawler();
     await crawler.init();
@@ -18,5 +23,18 @@ test('Scraper: random artist from search url', async () =>{
     expect(searchInfo.eventLink.startsWith('https://ra.co/events/')).toBe(true)
     expect(searchInfo.randomEventScLink.startsWith('https://www.soundcloud.com/')).toBe(true)
 
+}
+);
+
+test('Scraper: random track from artist soundcloud', async () =>{
+    let track = await getRandomSoundcloudTrack(testArtistScPage)
+    console.log(track)
+    // Many tracks will start with the artist url but not all - eg. if there is a collab
+    expect(track.startsWith('https://soundcloud.com/')).toBe(true)
+
+    // We want it to raise an error if it finds no tracks
+    // Not sure how to put this through with async functions
+    //expect(async() => {await getRandomSoundcloudTrack(errorUrl)}).toThrow()
+    
 }
 );
