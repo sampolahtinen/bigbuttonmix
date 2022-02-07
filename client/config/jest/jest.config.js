2b775e3ccb2c256d737b3e143ca593da4c5bed29
Sampo Lahtinen <sampo.lahtinen@endava.com>
2022-02-07 09:50:59 +0100

    [add] BigButton unit tests

--- client/config/jest/jest.config.js
+++ client/config/jest/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['../../src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
+  setupFilesAfterEnv: ['./setupTests.ts'],
   transform: {
-    '\\.(js|jsx|ts|tsx)$': ['esbuild-jest']
+    '\\.(js|jsx|ts|tsx)$': 'esbuild-jest'
   },
   testRegex: '/*.test.(ts|tsx|js)$',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/config/jest/assetsTransformer.ts',
-    '\\.svg': '<rootDir>/config/jest/svgrMock.ts'
+      '../../config/jest/assetsTransformer.ts',
+    '\\.svg': '../../config/jest/svgrMock.ts'
   }
 };
---
+++ client/src/components/BigButton/BgButton.test.tsx
@@ -0,0 +1,25 @@
+import { matchers } from '@emotion/jest';
+import { shallow } from 'enzyme';
+import React from 'react';
+import { BigButton, BigButtonContainer } from './BigButton';
+
+expect.extend(matchers);
+
+describe('BigButton', () => {
+  const mockClickHandler = jest.fn();
+  const wrapper = shallow(<BigButton onClick={mockClickHandler} />);
+  it('handles clicking', () => {
+    wrapper.find(BigButtonContainer).simulate('click');
+
+    expect(mockClickHandler).toHaveBeenCalled();
+  });
+
+  it('is small when isSmall prop is not passed into it', () => {
+    expect(wrapper.find(BigButtonContainer)).toHaveStyleRule('width', '240px');
+  });
+
+  it('is big when isSmall prop is passed into it', () => {
+    wrapper.setProps({ isSmall: true });
+    expect(wrapper.find(BigButtonContainer)).toHaveStyleRule('width', '120px');
+  });
+});
