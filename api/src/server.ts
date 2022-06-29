dba1618dd5eba1da5e99f3efd9cd7f7b24c31b49
Sampo Lahtinen <lampo.sahtinen@gmail.com>
2022-06-29 09:20:07 +0300

    fix: typo

--- api/src/server.ts
+++ api/src/server.ts
@@ -23,7 +23,7 @@ const redisClient = REDIS_ENABLED
 
 if (redisClient) {
   redisClient.on('ready', () =>
-    console.log(`${chalk.green('Redis connected successfully')}`)
+    console.log(`${chalk.green('Redis connected successfully!')}`)
   );
   redisClient.on('error', () =>
     console.log(`${chalk.red('Redis connection failed')}`)
